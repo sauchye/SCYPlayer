c62529430fbc10a8432404b111c29f4950b82f31
Sauchye <sauchyer@gmail.com>
2017-09-20 21:58:56 +0800

    Initial Commit

---
+++ SCYPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCYPlayer
+//
+//  Created by Saucheong Ye on 20/09/2017.
+//  Copyright © 2017 sauchye.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCYPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCYPlayer
+//
+//  Created by Saucheong Ye on 20/09/2017.
+//  Copyright © 2017 sauchye.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
